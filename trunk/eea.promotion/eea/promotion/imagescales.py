e628085aa07231d00ae3cefde4967e32890b20b5
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-05-28 10:12:35 +0000

    Added special ImageLink adapters for promotions. This is because we want to reuse same way of retrieving thumbnails and generating anchor links. Internally promoted videos are now marked as 'thickbox' and thus opens in a thickbox pop-up.
    Solves #2118

---
+++ trunk/eea.promotion/eea/promotion/imagescales.py
@@ -0,0 +1,48 @@
+from valentine.imagescales.browser.base import BaseImageLink
+from elementtree import ElementTree as ET
+
+
+class ImageLink(BaseImageLink):
+    """ImageLink for items in the promotion side bar
+
+
+    The markup mimics the template markup that was before. It's highly coupled
+    with the logic of promotion.js.dtml.
+
+    See #2118.
+    """ 
+
+    def __init__(self, context, request):
+        super(ImageLink, self).__init__(context, request)
+        self.classnames.append('portletHeader')
+
+    def link(self, imgtag):
+        a1 = ET.Element('a')
+        a1.set('title', self.desc)
+        a1.set('class', ' '.join(self.classnames))
+        a1.set('href', self.url)
+        a1.text = self.title
+
+        br = ET.SubElement(a1, 'br')
+        br.set('clear', 'all')
+
+        img = ET.XML(imgtag)
+
+        # This anchor is just to be read by showImage in promotion.js.dtml
+        a2 = ET.Element('a')
+        a2.set('style', 'display: none')
+        a2.set('href', img.get('src'))
+
+        a3 = ET.Element('a')
+        a3.set('class', 'noHover')
+        a3.set('title', self.desc or 'description not available')
+        a3.append(img)
+
+        return ET.tostring(a1) + ET.tostring(a2) + ET.tostring(a3)
+
+
+class VideoImageLink(ImageLink):
+
+    def __init__(self, context, request):
+        super(VideoImageLink, self).__init__(context, request)
+        self.classnames.append('thickbox')
