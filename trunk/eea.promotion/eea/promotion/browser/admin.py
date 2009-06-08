85e26b8642a1040d59ed991e7726881076708faa
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-06-08 08:23:35 +0000

    Removed unused stylesheet. Added back column for 'active'. Fixed bug where view was only available with site root as context

--- trunk/eea.promotion/eea/promotion/browser/admin.py
+++ trunk/eea.promotion/eea/promotion/browser/admin.py
@@ -1,5 +1,6 @@
 from zope.component import getMultiAdapter
 from zope.interface import implements
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from eea.promotion.interfaces import IPromoted, IPromotion
@@ -15,20 +16,25 @@ class AdminView(BrowserView):
 
     def find_promotions(self):
         ret = []
-        catalog = self.context.portal_catalog
-        query = {'object_provides': ['eea.promotion.interfaces.IPromoted', 'Products.EEAContentTypes.content.interfaces.IExternalPromotion'],
-                'review_state': 'published'}
+        catalog = getToolByName(self.context, 'portal_catalog')
+        query = {'object_provides': ['eea.promotion.interfaces.IPromoted',
+                 'Products.EEAContentTypes.content.interfaces.IExternalPromotion']}
         result = catalog.searchResults(query)
         for i in result:
             obj = i.getObject()
-            promo = IPromotion(obj)
+            try:
+                promo = IPromotion(obj)
+            except:
+                continue
             info = {'title': i.Title,
-                    'href': obj.absolute_url() + '/promotion_edit.html',
+                    'href': promo.edit_url,
                     'locations': u', '.join(promo.locations), 
                     'themes': u', '.join(promo.themes),
                     'frontpage_section': (promo.frontpage_section or '').split('/')[-1],
+                    'themepage_section': (promo.themepage_section or '').split('/')[-1],
                     'imgtag' : getMultiAdapter((obj, obj.REQUEST), name='imgtag'),
-                    'active': promo.active}
+                    'active': promo.active,
+                    'external': promo.is_external}
             ret.append(info)
         return ret
 
