1c20efa8dc168b8429bde3798d12588734970465
thuliper <thuliper@24821446-e7ea-0310-81ac-b189f4507b0b>
2009-06-08 10:09:43 +0000

    Removed setupstep for old admin view (RichTopic)

--- trunk/eea.promotion/eea/promotion/setuphandlers.py
+++ trunk/eea.promotion/eea/promotion/setuphandlers.py
@@ -1,11 +1,6 @@
 def setupManagePage(context):
-    portal = context.getSite()
-    if not hasattr(portal.SITE.cms, 'promotion_manage'):
-        portal.SITE.cms.invokeFactory('RichTopic', id='promotion_manage')
-        promotion_manage = portal.SITE.cms['promotion_manage']
-        promotion_manage.setTitle(u'Manage Promotions')
-        crit = promotion_manage.addCriterion('object_provides', 'ATSelectionCriterion')
-        crit.setValue('eea.promotion.interfaces.IPromoted')
+    # Removed due to new admin view. See #2469
+    pass
 
 
 def setupQuicklinks(context, portal=None):
