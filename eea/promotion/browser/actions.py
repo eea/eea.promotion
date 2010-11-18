573871cb119661a20eb40d28ff9cfa851cece94a
Sasha Vinčić <vincic@users.noreply.github.com>
2010-11-18 14:39:01 +0000

    Tagged eea.promotion 1.5.

--- eea/promotion/browser/actions.py
+++ eea/promotion/browser/actions.py
@@ -1,4 +1,3 @@
-from zope.component import queryMultiAdapter
 from zope.app.event.objectevent import ObjectModifiedEvent
 from zope.event import notify
 from zope.interface import alsoProvides, directlyProvides, directlyProvidedBy
@@ -19,11 +18,10 @@ class CreatePromotion(object):
         Promotions that don't have an promo_imglink adapter are blocked from this action.
         This is because this adapter is required for their listing.
         """
-        if queryMultiAdapter((self.context, self.request), name='promo_imglink') == None:
-            raise Exception, u"Sorry, no promo_imglink adapter available for object at " + self.context.absolute_url()
-        alsoProvides(self.context, IPromoted) 
-        notify(ObjectModifiedEvent(self.context))
-        self.context.reindexObject()
+        for obj in [self.context] + [i[0] for i in self.context.getTranslations().values()]:
+            alsoProvides(obj, IPromoted) 
+            notify(ObjectModifiedEvent(obj))
+            obj.reindexObject()
         return self.request.RESPONSE.redirect(self.context.absolute_url() + '/promotion_edit.html')
 
 
@@ -36,10 +34,30 @@ class RemovePromotion(object):
         self.request = request
 
     def __call__(self):
-        promo = IPromotion(self.context)
-        if promo.display_on_frontpage:
-            notify(ObjectModifiedEvent(self.context))
-        promo.remove()
-        directlyProvides(self.context, directlyProvidedBy(self.context) - IPromoted)
-        self.context.reindexObject()
-        return self.request.RESPONSE.redirect(self.context.absolute_url())
+        for obj in [self.context] + [i[0] for i in self.context.getTranslations().values()]:
+            if IPromoted.providedBy(obj):
+                promo = IPromotion(obj)
+                if promo.display_on_frontpage:
+                    notify(ObjectModifiedEvent(obj))
+                promo.remove()
+                directlyProvides(obj, directlyProvidedBy(obj) - IPromoted)
+                obj.reindexObject()
+        return self.request.RESPONSE.redirect(self.context.absolute_url() + '/edit?portal_status_message=Promotions removed from item and its translations')
+
+
+class PromoteTranslations(object):
+
+    """ """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self):
+        for trans in self.context.getTranslations().values():
+            obj = trans[0]
+            if not IPromoted.providedBy(obj):
+                alsoProvides(obj, IPromoted) 
+                notify(ObjectModifiedEvent(obj))
+                obj.reindexObject()
+        return self.request.RESPONSE.redirect(self.context.absolute_url() + '/edit?portal_status_message=Translations was promoted')
