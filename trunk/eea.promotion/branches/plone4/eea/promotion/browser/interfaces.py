8ee637c5a291ae0e03f47751a1bd06d24f90709f
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-05 07:45:45 +0000

    eea.promotions is used from another branch ont from trunk

--- trunk/eea.promotion/branches/plone4/eea/promotion/browser/interfaces.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-
-
-class IAdminView(Interface):
-
-    """ Custom view that gives an overview of all internal promotions on the site. """
-
-    def find_promotions(): pass
