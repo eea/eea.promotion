a72648846b88c7b97e41381765426bec0d0ea371
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-08-01 15:22:49 +0000

    Refs #14863; added form with button to disable inactive promotions in order to speed up promotions lookup by not having to wake up unnecesary objects within promotion_admin_view

--- trunk/eea.promotion/trunk/eea/promotion/browser/admin.py
+++ trunk/eea.promotion/trunk/eea/promotion/browser/admin.py
@@ -1,37 +1,47 @@
 """ Admin
 """
-from zope.interface import implements
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.interface import implements, noLongerProvides
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from eea.promotion.interfaces import IPromotion #IPromoted
+from eea.promotion.interfaces import IPromotion, IPromoted
 from eea.promotion.browser.interfaces import IAdminView
 from DateTime.DateTime import DateTime
+
 
 class AdminView(BrowserView):
     """ Admin view
     """
     implements(IAdminView)
+    template = ViewPageTemplateFile('admin.pt')
+
 
     def __init__(self, context, request):
         BrowserView.__init__(self, context, request)
         self.context = context
         self.request = request
 
+    def current_promotions(self):
+        """ return the brains with promotions from site
+        """
+        now = DateTime()
+        catalog = getToolByName(self.context, 'portal_catalog')
+        result = catalog({
+            'object_provides': [
+               'eea.promotion.interfaces.IPromoted',
+               'Products.EEAContentTypes.content.interfaces.IExternalPromotion',
+            ],
+            'review_state': 'published',
+            'effectiveRange': now,
+            })
+        return result
+
     def find_promotions(self):
         """ Find promotions
         """
         ret = []
-        now = DateTime()
-        catalog = getToolByName(self.context, 'portal_catalog')
-        result = catalog({
-          'object_provides': [
-            'eea.promotion.interfaces.IPromoted',
-            'Products.EEAContentTypes.content.interfaces.IExternalPromotion',
-          ],
-            'review_state': 'published',
-            'effectiveRange': now,
-        })
+        result = self.current_promotions()
         for brain in result:
             obj = brain.getObject()
             try:
@@ -52,4 +62,25 @@ class AdminView(BrowserView):
             })
         return ret
 
-    __call__ = ViewPageTemplateFile('admin.pt')
+    def disable_inactive_promotions(self):
+        """ Disable all inactive promotions
+        """
+        result = self.current_promotions()
+        for brain in result:
+            obj = brain.getObject()
+            try:
+                promo = IPromotion(obj)
+            except Exception:
+                continue
+            if not promo.active:
+                noLongerProvides(obj, IPromoted)
+                obj.reindexObject(idxs=['object_provides'])
+        msg = "Inactive Promotions have been succesfully disabled"
+        IStatusMessage(self.request).addStatusMessage(msg, type='info')
+        return self.request.response.redirect(self.context.absolute_url() +
+                                              '/@@promotion_admin_view')
+
+    def __call__(self):
+        if "submit" not in self.request.form:
+            return self.template()
+        return self.disable_inactive_promotions()
